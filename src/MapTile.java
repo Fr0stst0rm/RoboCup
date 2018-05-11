84e94b938608cef6a64fb7af7c52c26ab9e423f5
Christoph Cudly <christoph.cudly@gmail.com>
2018-05-11 12:38:37 +0200

    Map und Tiles

---
+++ src/MapTile.java
@@ -0,0 +1,13 @@
+
+public class MapTile {
+	
+	public boolean wallNorth = false;
+	public boolean wallEast = false;
+	public boolean wallSouth = false;
+	public boolean wallWest = false;
+	
+	public boolean isWalkable = true;
+	
+	public boolean filled = false;
+
+}
